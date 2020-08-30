d17f32dd0ca3ba395025c1a02d4dbb6f8d9fae09
Filip Ceglik <fceglik@gmail.com>
2020-08-30 16:53:18 +0200

    ApiService working

---
+++ CovidMap/ApiService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace CovidMap
+{
+    public class ApiService
+    {
+        private readonly RestClient _restService = new RestClient("https://covid19-api.com/country/");
+        public IEnumerable<CovidData> GetLatestReportByCountryCode()
+        {
+            var request = new RestRequest("code", Method.GET)
+                .AddParameter("format","json")
+                .AddParameter("code","it")
+                .AddHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
+                .AddHeader("x-rapidapi-key", "no api key for you dear friend");
+            var response = _restService.Execute<IEnumerable<CovidData>>(request);
+
+            if (!response.IsSuccessful)
+            {
+                return null;
+            }
+
+            return response.Data;
+        }
+    }
+    public class CovidData
+    {
+        public IEnumerable<CountrySummary> Daily { get; set; }
+    }
+
+    public class Country
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class CountrySummary
+    {
+        public int confirmed { get; set; }
+        public int recovered { get; set; }
+        public int critical { get; set; }
+        public int deaths { get; set; }
+        public Country country { get; set; }
+        public DateTime lastChange { get; set; }
+        public DateTime lastUpdate { get; set; }
+    }
+}
--- CovidMap/MainWindow.xaml.cs
+++ CovidMap/MainWindow.xaml.cs
@@ -1,13 +1,18 @@
-﻿namespace CovidMap
+﻿using System;
+
+namespace CovidMap
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private readonly ApiService _apiService = new ApiService();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = _apiService.GetLatestReportByCountryCode();
+            Console.WriteLine();
         }
     }
 }
